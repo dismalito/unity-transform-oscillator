3f656ff6cdff5958d7dc0679e7566dc738250d65
dismalito <dismal.cj@gmail.com>
2024-05-29 14:35:18 -0300

    add scale oscillator

---
+++ Assets/TransformOscillator/Scripts/ScaleOscillator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace TransformOscillator.Scripts
+{
+    public class ScaleOscillator : MonoBehaviour
+    {
+        [SerializeField] private Transform _pivot;
+        [SerializeField] internal Vector3 _targetScale = new Vector3(2,2,2);
+        [SerializeField] private float _speed = 1f;
+        [SerializeField] private OscillatorType _type;
+        [SerializeField] private AnimationCurve _animationCurve;
+        [SerializeField] private bool _playOnStart = true;
+
+        private bool _activated = false;
+        private Vector3 _defaultScale;
+
+        private void Awake()
+        {
+            _defaultScale = _pivot.localScale;
+        }
+
+        private void Start()
+        {
+            if (_playOnStart)
+                Play();
+        }
+
+        public void Play()
+        {
+            _activated = true;
+        }
+
+        public void Stop()
+        {
+            _activated = false;
+        }
+
+        private void Update()
+        {
+            if (!_activated) return;
+
+            var time = GetTimeFromMovementType();
+            var offset = _animationCurve.Evaluate(time);
+            var scale = Vector3.Lerp(_defaultScale, _targetScale, offset);
+            _pivot.localScale = scale;
+        }
+
+        private float GetTimeFromMovementType() => _type == OscillatorType.Oscillator
+            ? Mathf.PingPong(Time.time * _speed, 1)
+            : Mathf.Repeat(Time.time * _speed, 1);
+    }
+}
